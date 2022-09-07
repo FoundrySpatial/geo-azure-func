409ef698a0e13e50e221d786c2b6d9ab517c0eb7
jhamidi <jam@foundryspatial.com>
2022-09-07 09:10:30 -0700

    work in progress

---
+++ scripts/package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# directory used for deployment
+export DEPLOY_DIR=lambda
+
+# make deployment directory and add lambda handler
+mkdir -p $DEPLOY_DIR/lib
+
+# copy libs
+cp -P ${PREFIX}/lib/*.so* $DEPLOY_DIR/lib/
+cp -P ${PREFIX}/lib64/libjpeg*.so* $DEPLOY_DIR/lib/
+
+strip $DEPLOY_DIR/lib/* || true
+
+# copy GDAL_DATA files over
+mkdir -p $DEPLOY_DIR/share
+rsync -ax $PREFIX/share/gdal $DEPLOY_DIR/share/
+rsync -ax $PREFIX/share/proj $DEPLOY_DIR/share/
+
+# zip up deploy package
+cd $DEPLOY_DIR
+zip --symlinks -ruq ../lambda-deploy.zip ./

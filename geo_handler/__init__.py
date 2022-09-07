470595256ed0d4beefbdfe4f677cfa3b138b7847
jhamidi <jam@foundryspatial.com>
2022-09-07 14:24:26 -0700

    fix to return error

--- geo_handler/__init__.py
+++ geo_handler/__init__.py
@@ -23,7 +23,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         stats = band.GetStatistics(0, 1)
 
     return stats 
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse("Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
